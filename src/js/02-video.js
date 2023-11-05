6f917c20e838ded4fe16e6072738944f9e5efe74
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2023-11-05 16:30:18 +0200

    додає виконання завдання 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const ifrimeEl = document.querySelector('iframe');
+const player = new Player(ifrimeEl);
+
+savedTime();
+
+function onTimeUpdate(event) {
+  localStorage.setItem(LOCALSTORAGE_KEY, `${event.seconds}`);
+}
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function savedTime() {
+  let currentTime = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+}
 
